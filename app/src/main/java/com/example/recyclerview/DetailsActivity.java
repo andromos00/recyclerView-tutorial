3ebef1ed4a03026ad04581e6fcf4164912e44939
Rozan <xrozan@gmail.com>
2021-01-11 20:44:07 +0700

    1/11/2021 Tutorial 5 Done

---
+++ app/src/main/java/com/example/recyclerview/DetailsActivity.java
@@ -0,0 +1,51 @@
+package com.example.recyclerview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private String url;
+    private String name;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        name = getIntent().getStringExtra("name");
+        url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
+        Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello, this is Rozan Nauf with StudentID: 1201502580\n\n This is the link for " + name + " at\n\n" + url);
+                startActivity(intent);
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
